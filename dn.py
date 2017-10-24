ca6c97064be4510feec551fbdf42bdc7aafc489e
bossanc <nejcselan@gmail.com>
2017-10-24 13:12:14 +0200

    fizzbuzz a little more fun, funny if answer is outside parameters

--- dn.py
+++ dn.py
@@ -1,10 +1,20 @@
-n = raw_input("Pick a number from 1 to 100: ")
-for n in range(1, (int(n) + 1)):
-    if n % 5 == 0 and n % 3 == 0:
-        print "FizzBuzz"
-    elif n % 3 == 0:
-       print "Fizz"
-    elif n % 5 == 0:
-        print "Buzz"
-    else:
-        print n
+print "Welcome to the fizzbuzz generator!"
+
+n = int(raw_input("Pick a number from 1 to 100: "))
+if n > 100 or n < 1:
+    print "Are you high right now? (yes, no)"
+    answer = raw_input()
+    if answer == "yes":
+        print "THIS IS ALL AN ILLUSION"
+    elif answer == "no":
+        print "Try to get it right next time!"
+else:
+    for n in range(1, (n + 1)):
+        if n % 5 == 0 and n % 3 == 0:
+            print "FizzBuzz"
+        elif n % 3 == 0:
+           print "Fizz"
+        elif n % 5 == 0:
+            print "Buzz"
+        else:
+            print n
